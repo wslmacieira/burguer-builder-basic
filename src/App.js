ef79f16bee494104243f40f7b60d1e33c68189dc
wslmacieira <wslmacieira@outlook.com>
2019-09-14 00:36:57 -0300

    cretae Layout component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+
+import Layout from './components/Layout/Layout'
 
 class App extends Component {
   render() {
     return (
       <div>
-
+        <Layout>
+          <p>Test</p>
+        </Layout>
       </div>
     );
   }
 }
 
-export default App;
+export default App
